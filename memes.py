501fef2cf7ed7fdabfa0d4c35fb268976df20f07
musiczhara0 <zharamusic9@gmail.com>
2024-09-25 15:49:11 +0300

    Update memes.py

--- memes.py
+++ memes.py
@@ -17,6 +17,29 @@ class memes(loader.Module):
         """Иди бери ёршик чисти унитаз"""
         await self.send_voice(message, "https://telesco.pe/SosatXuyEtoXorosho/26")
 
+    async def пидорасcmd(self, message: Message):
+        """Слыш ты пидорас, отсосёшь только ты"""
+        await self.send_voice(message, "https://telesco.pe/SosatXuyEtoXorosho/27")
+
+    async def боксcmd(self, message: Message):
+        """Урок бокса"""
+        await self.send_voice(message, "https://telesco.pe/SosatXuyEtoXorosho/28")
+
+    async def бабушкаcmd(self, message: Message):
+        """Что теплое в вмертвой бабушке ?"""
+        await self.send_voice(message, "https://telesco.pe/SosatXuyEtoXorosho/29")
+
+    async def взрослаяcmd(self, message: Message):
+        """Забыла у тебя спросить"""
+        await self.send_voice(message, "https://telesco.pe/SosatXuyEtoXorosho/30")
+
+    async def нахуйcmd(self, message: Message):
+        """Пошёл ты нахуй, я не буду плакать"""
+        await self.send_voice(message, "https://telesco.pe/SosatXuyEtoXorosho/31")
+
+    async def петардаcmd(self, message: Message):
+        """Единственная петарда которая взорвёться в этом году"""
+        await self.send_voice(message, "https://telesco.pe/SosatXuyEtoXorosho/32")
 
     
     async def send_voice(self, message: Message, link: str):
