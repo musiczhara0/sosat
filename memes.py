83285c4250067f4f0bb951efb33a287fcdb21441
musiczhara0 <zharamusic9@gmail.com>
2024-09-25 15:33:55 +0300

    Add files via upload

---
+++ memes.py
@@ -0,0 +1,31 @@
+__version__ = (1, 0, 0)
+# meta developer: @musiczhara0
+# for more info: https://github.com/musiczhara0/sosat/blob/main/Idi%20naxuy.py
+from .. import loader  # Импортируем необходимые модули
+from telethon.tl.types import Message
+
+@loader.tds
+class memes(loader.Module):
+    """Мемы и кружки"""
+    
+    strings = {
+        "name": "Memes",
+        "developer": "Разработчик: musiczhara0"
+    }
+
+    async def ёршикcmd(self, message: Message):
+        """Иди бери ёршик чисти унитаз"""
+        await self.send_voice(message, "https://telesco.pe/SosatXuyEtoXorosho/26")
+
+
+    
+    async def send_voice(self, message: Message, link: str):
+        """Общий метод для отправки голосового сообщения по ссылке"""
+        reply = await message.get_reply_message()
+        await message.delete()
+        await message.client.send_file(
+            message.to_id,
+            link,
+            voice_note=True,
+            reply_to=reply.id if reply else None,
+        )
