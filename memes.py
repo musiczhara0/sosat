fc6f17a7e8de495a8f5a5f741285a139bcc98d0f
musiczhara0 <zharamusic9@gmail.com>
2024-09-26 11:27:22 +0300

    Update memes.py

--- memes.py
+++ memes.py
@@ -1,4 +1,4 @@
-__version__ = (1, 0, 2)
+__version__ = (1, 0, 3)
 # meta developer: @musiczhara0
 # for more info: https://raw.githubusercontent.com/musiczhara0/sosat/refs/heads/main/memes.py
 from .. import loader  # Импортируем необходимые модули
@@ -45,15 +45,15 @@ class memes(loader.Module):
         """Скинь хуй"""
         await self.send_voice(message, "https://telesco.pe/SosatXuyEtoXorosho/33")
 
-     async def физкультcmd(self, message: Message):
+    async def физкультcmd(self, message: Message):
         """Физкульт зарядка под ебейший бит"""
         await self.send_voice(message, "https://t.me/SosatXuyEtoXorosho/34")
 
-     async def выхлопcmd(self, message: Message):
+    async def выхлопcmd(self, message: Message):
         """Я вообще делаю что хочу - ебейший выхлоп"""
         await self.send_voice(message, "https://t.me/SosatXuyEtoXorosho/34")
 
-      async def send_voice(self, message: Message, link: str):
+     async def send_voice(self, message: Message, link: str):
         """Общий метод для отправки голосового сообщения по ссылке"""
         reply = await message.get_reply_message()
         await message.delete()
